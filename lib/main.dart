12ae85ba6057d40ef08089f3f82500f2917eca9e
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-10 05:22:53 +0300

    instagram app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_app/colors.dart';
 import 'package:instagram_app/dementaion.dart';
@@ -5,7 +6,10 @@ import 'package:instagram_app/responsive/mobile_screen_layout.dart';
 import 'package:instagram_app/responsive/responsive_layout_screen.dart';
 import 'package:instagram_app/responsive/web_screen_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
