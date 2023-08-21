f57dbd0637601831e48c0492f817fe891f0be6d8
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-21 21:01:27 +0300

    instagram app 5

--- lib/widgets/comment-card.dart
+++ lib/widgets/comment-card.dart
@@ -10,6 +10,23 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1692211987300-86fefbfb3131?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"),
+            radius: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
