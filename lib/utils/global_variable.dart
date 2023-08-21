d2cb90e3d1ad41824a6876c4d0b6cd87b46166d5
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-21 04:15:26 +0300

    instagram app 4

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_app/screens/add_post_screens.dart';
+import 'package:instagram_app/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreensItems = [
-  Text("read"),
+  FeedScreen(),
   Text("search"),
   AddPostScreens(),
   Text("notification"),
---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Container();
+  }
+}
