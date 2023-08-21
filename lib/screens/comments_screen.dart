53b827fd5f88221d20d84686ed2ebd0fd1db633c
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-21 20:39:05 +0300

    instagram app 5

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_app/utils/colors.dart';
+import 'package:instagram_app/widgets/comment-card.dart';
 
 class CommentsScreen extends StatefulWidget {
   const CommentsScreen({super.key});
@@ -16,6 +17,48 @@ class _CommentsScreenState extends State<CommentsScreen> {
         backgroundColor: mobileBackgroundColor,
         title: Text("Comments"),
         centerTitle: false,
+      ),body: CommentCard(),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          padding: EdgeInsets.only(
+            left: 16,
+            right: 8,
+          ),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1682685797898-6d7587974771?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"),
+                radius: 10,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Comment as username",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: Text(
+                    "Post",
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
