5b043cb03579287cbca1286dcd615b48ef5cef7a
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-21 04:25:08 +0300

    instagram app 4

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/utils/colors.dart';
+
 class PostCard extends StatelessWidget {
   const PostCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Container();
+    return Container(
+      color: mobileBackgroundColor,
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                .copyWith(right: 0),
+            child: const Row(
+              children: [
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(
+                      "https://plus.unsplash.com/premium_photo-1677637688405-fa8a3cb3597c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "username",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
