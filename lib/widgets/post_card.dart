1a0f5523b601f47f1f5bf928220a83fbe42f8f02
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-21 04:37:49 +0300

    instagram app 4

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -14,7 +14,7 @@ class PostCard extends StatelessWidget {
           Container(
             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
                 .copyWith(right: 0),
-            child: const Row(
+            child: Row(
               children: [
                 CircleAvatar(
                   radius: 16,
@@ -34,9 +34,72 @@ class PostCard extends StatelessWidget {
                       )
                     ],
                   ),
-                ))
+                )),
+                IconButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => Dialog(
+                                child: ListView(
+                                  padding: EdgeInsets.symmetric(vertical: 16),
+                                  shrinkWrap: true,
+                                  children: ["Delete"]
+                                      .map(
+                                        (e) => InkWell(
+                                          onTap: () {},
+                                          child: Container(
+                                            padding: EdgeInsets.symmetric(
+                                                vertical: 12, horizontal: 16),
+                                            child: Text(e),
+                                          ),
+                                        ),
+                                      )
+                                      .toList(),
+                                ),
+                              ));
+                    },
+                    icon: Icon(Icons.more_vert))
               ],
             ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .35,
+            width: double.infinity,
+            child: Image.network(
+              "https://images.unsplash.com/photo-1594165681163-1b85559cf250?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.comment_outlined,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.send,
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomRight,
+                child: IconButton(
+                  icon: Icon(Icons.bookmark_border),
+                  onPressed: () {},
+                ),
+              ))
+            ],
           )
         ],
       ),
