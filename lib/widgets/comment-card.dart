826caf3b2be3ca5fd37b7061082fb2494df31f5a
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-08-21 22:22:17 +0300

    instagram app 5

--- lib/widgets/comment-card.dart
+++ lib/widgets/comment-card.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/provider/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_app/models/user.dart' as models;
 
 class CommentCard extends StatefulWidget {
   const CommentCard({super.key});
@@ -10,19 +13,51 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
+    final models.User user = Provider.of<UserProvider>(context).getUser;
     return Container(
       padding: EdgeInsets.symmetric(vertical: 18, horizontal: 16),
       child: Row(
         children: [
           CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://images.unsplash.com/photo-1692211987300-86fefbfb3131?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"),
+            backgroundImage: NetworkImage(user.photoUrl),
             radius: 10,
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                      text: "username",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: "some description to insert",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ])),
+                  Padding(
+                    padding: EdgeInsets.only(top: 4),
+                    child: Text(
+                      "23/12/2",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child: Icon(
+                      Icons.favorite,
+                      size: 16,
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
